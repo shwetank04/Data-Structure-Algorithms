48e0e8863c840e47ec544d05fcf65541106be597
shwetank <56911869+shwetank04@users.noreply.github.com>
2024-11-04 19:40:11 +0530

    Update Divisor.java

--- Basic Maths/Divisor.java
+++ Basic Maths/Divisor.java
@@ -12,3 +12,24 @@ public class Solution{
         return divisors;
     }
 }
+
+
+// import java.util.ArrayList;
+// import java.util.Collections;
+// import java.util.List;
+// public class Solution{
+//     public static List< Integer > printDivisors(int n) {
+//         // Write your code here
+//         List<Integer> divisors = new ArrayList<>();
+//         for(int i=1;i<=Math.sqrt(n);i++){
+//         if(n%i == 0){
+//             divisors.add(i);
+//             if(n/i !=i){
+//                 divisors.add(n/i);
+//             }
+//         }
+//         }
+//          Collections.sort(divisors);
+//          return divisors;
+//     }
+// }

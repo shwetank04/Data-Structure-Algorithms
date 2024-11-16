8e750bf964371c6061256669b5a388af40d124bc
shwetank <56911869+shwetank04@users.noreply.github.com>
2024-11-16 22:59:40 +0530

    Create LeftRotateByOne.java

---
+++ Arrays/LeftRotateByOne.java
@@ -0,0 +1,12 @@
+public class Solution {
+
+    static int[] rotateArray(int[] arr, int n) {
+        // Write your code here.
+        int temp = arr[0];
+        for(int i=0;i<n-1;i++){
+            arr[i] = arr[i+1];
+        }
+        arr[n-1] = temp;
+        return arr;
+    }
+}

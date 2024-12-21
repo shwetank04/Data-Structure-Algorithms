405e70c66637acbceac2eda3c8abb47494b5f46e
shwetank <56911869+shwetank04@users.noreply.github.com>
2024-12-21 16:06:54 +0530

    Create AssignCookies.java

---
+++ GreedyAlgorithm/AssignCookies.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+       int i = 0;
+       int j = 0;
+       int result =0;
+       Arrays.sort(g);
+       Arrays.sort(s);
+       while(i<g.length && j<s.length){
+            if(s[j] >= g[i]){
+                i++;
+            }
+               j++;
+       }
+       return i;
+    }
+}
